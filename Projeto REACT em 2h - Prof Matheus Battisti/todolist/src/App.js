3ac5df87a8769599e03fdecbdc281b827da4e6e8
toni <antoniojsereno@gmail.com>
2023-03-08 13:16:32 -0300

    imports necessarios e definiçao dos States que seram utilizados

--- Projeto REACT em 2h - Prof Matheus Battisti/todolist/src/App.js
+++ Projeto REACT em 2h - Prof Matheus Battisti/todolist/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import {BsTrash, BsBookmarckCheck, BsBookmarckCheckFill } from 'react-icons/bs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API = "http://localhost:5000";
+
+export default function App(){
+
+    const [title, setTitle] =  useState("");
+    const [time, setTime] = useState("");
+    const [todos, setTodos] = useState({}); //aqui inicia ocm uma lista vazia para que eu possa colocar objetos TO-DO´s dentro dessa array.
+    const[loading, setLoading] = useState(false);
+
+    
+
+    return (
+        <>
+            <div className='App'>
+                <h1>TO-DO´s</h1>
+            </div>
+        </>
+    );
 }
-
-export default App;
