998505365faff108a324f24d5cc169413113a714
toni <antoniojsereno@gmail.com>
2023-03-08 13:27:12 -0300

    css inicial

--- Projeto REACT em 2h - Prof Matheus Battisti/todolist/src/App.js
+++ Projeto REACT em 2h - Prof Matheus Battisti/todolist/src/App.js
@@ -11,12 +11,20 @@ export default function App(){
     const [todos, setTodos] = useState({}); //aqui inicia ocm uma lista vazia para que eu possa colocar objetos TO-DO´s dentro dessa array.
     const[loading, setLoading] = useState(false);
 
-    
+
 
     return (
         <>
             <div className='App'>
-                <h1>TO-DO´s</h1>
+                <div className='todo-header'>
+                    <h1>Projeto TO-DO´s React</h1>
+                </div>
+                <div className='form-todo'>
+                    <p>Fomrulario</p>
+                </div>
+                <div className='list-todo'>
+                    <p>lista de TO-DOs</p>
+                </div>
             </div>
         </>
     );
