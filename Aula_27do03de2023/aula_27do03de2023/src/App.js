e080e7038bcb1e3c7902fa892770dfea672145e2
Antonio Sereno <antoniojsereno@gmail.com>
2023-03-27 09:08:24 -0300

    fim de aula, amanha tem mais.

--- Aula_27do03de2023/aula_27do03de2023/src/App.js
+++ Aula_27do03de2023/aula_27do03de2023/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 
+const myDiv =  <>
+<h3 align="center">Meu Programa</h3>
+<br/>
+<span><strong>Lista</strong></span>
+<ul>
+  <li>Item 1</li>
+  <li>Item 2</li>
+  <li>Item 3</li>
+  <li>Item 4</li>
+  </ul>
+</>
+
 function App() {
   return (
-    <>
-      <h1>Meu Programa</h1>
-    </>
+   myDiv
   );
 }
 
